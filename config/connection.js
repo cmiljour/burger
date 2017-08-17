5c375dbf83451260a09d327b58309d84fcf7da32
cmiljour <cormil@gmail.com>
2017-08-17 10:14:34 -0500

    adjusted mariadb connection string

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 var mysql = require("mysql");
 
-if (process.env.JAWSDB_URL){
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
+if (process.env.JAWSDB_MARIA_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_MARIA_URL);
 } else {
   connection = mysql.createConnection({
     host: "localhost",
